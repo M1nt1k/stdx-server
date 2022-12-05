abee3a24d25e0beff7faa8fc05329fe59eb3d431
Maxim <58911169+M1nt1k@users.noreply.github.com>
2022-12-05 21:18:10 +0500

    Delete stdx/exchange directory

--- stdx/exchange/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Task)
-admin.site.register(Category)
-admin.site.register(Owner)
-admin.site.register(University)
-admin.site.register(Files)
